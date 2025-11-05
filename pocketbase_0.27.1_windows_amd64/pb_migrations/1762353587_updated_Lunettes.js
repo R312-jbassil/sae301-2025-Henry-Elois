3643772db445ec755577e997048107afb8eda987
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-11-05 15:40:26 +0100

    gallery

---
+++ pocketbase_0.27.1_windows_amd64/pb_migrations/1762353587_updated_Lunettes.js
@@ -0,0 +1,26 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3758190954")
+
+  // update collection data
+  unmarshal({
+    "deleteRule": "@request.auth.id = id_client",
+    "listRule": "@request.auth.id != \"\"",
+    "updateRule": "@request.auth.id = id_client",
+    "viewRule": "@request.auth.id != \"\""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3758190954")
+
+  // update collection data
+  unmarshal({
+    "deleteRule": null,
+    "listRule": null,
+    "updateRule": null,
+    "viewRule": null
+  }, collection)
+
+  return app.save(collection)
+})
