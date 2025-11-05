d07f0feb3e9caa8938d8af014913af9b0588d8b7
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-11-05 16:57:56 +0100

    Remove conflicting tailwind dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
 export default defineConfig({
-  output: "server", // Mode SSR
+  output: "server",
   adapter: node({
     mode: "standalone",
   }),
-  integrations: [tailwind()],
 });
