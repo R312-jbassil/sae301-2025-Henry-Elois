528744eda358b3d5995ebf02be7b26753df38f3c
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-11-05 16:44:31 +0100

    deploiement

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,9 +1,16 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-types";
 
+var path = "";
+if (import.meta.env.MODE === "development")
+  path = "http://localhost:8090"; //localhost = machine de dev
+else path = "https://tavue.eloishenry.fr:443"; //url du site
 const PB_URL =
   process.env.PB_URL || import.meta.env.PB_URL || "http://127.0.0.1:8090/";
 
 const pb = new PocketBase(PB_URL) as TypedPocketBase;
 
 export default pb;
+
+
+
