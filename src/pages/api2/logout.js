629b765fa9d754d8a45c32b17da16988e2027514
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-11-06 11:55:40 +0100

    Fix logout cookie domain fallback to use default domain

--- src/pages/api2/logout.js
+++ src/pages/api2/logout.js
@@ -3,11 +3,11 @@ export const POST = async ({ locals, cookies }) => {
   pb.authStore.clear();
   cookies.delete("pb_auth", {
     path: "/",
-    domain: process.env.COOKIE_DOMAIN || undefined, // set your domain if needed
+    domain: process.env.COOKIE_DOMAIN || "eloishenry.com", // set your domain if needed
     secure: true, // use true if your site is HTTPS in production
     sameSite: "strict",
   });
-  
+
   return new Response(null, {
     status: 303,
     headers: {
